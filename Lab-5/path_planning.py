e646ee37c1fb03c337867e6ae74553ba2b44d1a0
Ian Lo <ianlo196@gmail.com>
2017-02-18 09:37:37 -0500

    Dijkstra's should be working now

--- Lab-5/path_planning.py
+++ Lab-5/path_planning.py
@@ -11,99 +11,101 @@ BLOCK_WIDTH = 2
 WIDTH_IDX = WIDTH // resolution
 HEIGHT_IDX = HEIGHT // resolution
 
-def inToIdx(x):
-    return int(x/resolution)
+DIRECTIONS = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']
+
 
 grid = [[0 for x in range(WIDTH_IDX)] for y in range(HEIGHT_IDX)]
 blocks = [
-    (12, 36, 36, 38),
-    (47, 48, 49, 26),
-    (38, 24, 58, 26),
-    (66, 48, 82.75, 28.5),
-    (60, 12, 73.25, 20.5),
-    (12, 24, 31.25, 13.5)
+    (12, 36, 36, 38), # Block 1
+    (47, 48, 49, 26), # Block 2
+    (38, 24, 58, 26), # Block 3
+    (66, 48, 82.75, 28.5), # Block 4
+    (60, 12, 73.25, 20.5), # Block 5
+    (12, 24, 31.25, 13.5) # Block 6
 ]
-for row in range(len(grid)):
-    for col in range(len(grid[row])):
-        x0 = col * resolution
-        y0 = row * resolution
-        wall = 0
-        for (x1, y1, x2, y2) in blocks:
-            d = (abs((x2 - x1) * (y1 - y0) - (x1 - x0) * (y2 - y1)) /
-                math.sqrt((x2 - x1)**2 + (y2 - y1)**2))
-            if (d < BLOCK_WIDTH + ROBOT_RADIUS and
-                min(x1, x2) <= x0 and x0 <= max(x1, x2) and
-                min(y1, y2) <= y0 and y0 <= max(y1, y2)):
-                wall = -1
-                break
-        grid[row][col] = wall
 
-grid[inToIdx(END[1])][inToIdx(END[0])] = 2
-counter = 2
-formatS = ' '.join(['%03d ' for x in range(WIDTH_IDX)])
+WR = 4
+waypoints = {
+    # Block 1 waypoints
+    'w11': (blocks[0][0]-WR, blocks[0][3]+WR),
+    'w12': (blocks[0][2]+WR, blocks[0][3]+WR),
+    'w13': (blocks[0][2]+WR, blocks[0][1]-WR),
+    'w14': (blocks[0][0]-WR, blocks[0][1]-WR),
+    # No waypoints for block 2
+    # Block 3 waypoints
+    'w31': (blocks[2][0]-WR, blocks[2][3]+WR),
+    'w32': (blocks[2][2]+WR, blocks[2][3]+WR),
+    'w33': (blocks[2][2]+WR, blocks[2][1]-WR),
+    'w34': (blocks[2][0]-WR, blocks[2][1]-WR),
+    # Block 4 waypoints
+    'w41': (86, 28),
+    'w42': (82.75, 24.5),
+    # Block 5 waypoints
+    'w51': (56, 12),
+    'w52': (70, 26),
+    'w53': (78, 20.5),
+    'w54': (62, 6),
+    # Block 6 waypoints
+    'w61': (12, 28),
+    'w62': (35, 16),
+    'w63': (31, 7),
+    'w64': (6, 22)
+}
 
-for row in range(len(grid)-1, -1, -1):
-    print(formatS % tuple(grid[row]))
-print('\n\n')
-while grid[inToIdx(START[1])][inToIdx(START[0])] == 0:
-    # Iterate through the grid
-    for row in range(len(grid)):
-        for col in range(len(grid[row])):
-            # If the location == counter, increment the places around it.
-            # (8-point connectivity)
-            if grid[row][col] == counter:
-                for i in range(0, 3):
-                    for j in range(0, 3):
-                        if (i != j and
-                            0 <= col + i - 1 and col + i - 1 < WIDTH_IDX and
-                            0 <= row + j - 1 and row + j - 1 < HEIGHT_IDX and
-                            grid[row + j - 1][col + i - 1] == 0):
-                            grid[row + j - 1][col + i - 1] = counter + 1
-    counter = counter + 1
+connections = {
+    # Block 1 waypoints
+    'w11': ['w12', 'w14'],
+    'w12': ['w13', 'w11'],
+    'w13': ['w12', 'w31', 'w61', 'w14'],
+    'w14': ['w11', 'w13', 'w31', 'w34', 'w62', 'w61', 'w64'],
+    # No waypoints for block 2
+    # Block 3 waypoints
+    'w31': ['w13', 'w34', 'w61', 'w14'],
+    'w32': ['w42', 'w52', 'w33'],
+    'w33': ['w32', 'w52', 'w51', 'w34'],
+    'w34': ['w31', 'w33', 'w51', 'w62', 'w61', 'w14'],
+    # Block 4 waypoints
+    'w41': ['w42'],
+    'w42': ['w41', 'w53', 'w52', 'w32'],
+    # Block 5 waypoints
+    'w51': ['w33', 'w52', 'w54', 'w63', 'w62', 'w61', 'w34'],
+    'w52': ['w42', 'w53', 'w33', 'w32'],
+    'w53': ['w42', 'w54', 'w52'],
+    'w54': ['w53', 'w63', 'w62', 'w34', 'w51'],
+    # Block 6 waypoints
+    'w61': ['w13', 'w31', 'w34', 'w51', 'w62', 'w64', 'w14'],
+    'w62': ['w34', 'w51', 'w54', 'w63', 'w61', 'w14'],
+    'w63': ['w62', 'w51', 'w54', 'w64'],
+    'w64': ['w14', 'w61', 'w63']
+}
 
-for row in range(len(grid)-1, -1, -1):
-    print(formatS % tuple(grid[row]))
+nodes = {
+    'w11':([], -1), 'w12':([], -1), 'w13':([], -1), 'w14':([], -1),
+    'w31':([], -1), 'w32':([], -1), 'w33':([], -1), 'w34':([], -1),
+    'w41':([], -1), 'w42':([], -1),
+    'w51':([], -1), 'w52':([], -1), 'w53':([], -1), 'w54':([], -1),
+    'w61':([], -1), 'w62':([], -1), 'w63':([], -1), 'w64':([], -1)
+}
 
-path = []
-distToGoal = grid[inToIdx(START[1])][inToIdx(START[0])]
-current = grid[inToIdx(END[1])][inToIdx(END[0])]
-
-def findPath(prevPath, row, col):
-    length = grid[row][col]
-    newPath = prevPath[:].append((row, col))
-    if length == distToGoal:
-        return newPath
-    # Try moving straight
-    for i in range(0, 3):
-        r = row + i - 1
-        c = col
-        if (0 <= r and r < HEIGHT_IDX and grid[r][c] == length + 1):
-            p = findPath(newPath, r, c)
-            if len(p) != 0:
-                return p
-        r = row
-        c = col + i - 1
-        if (0 <= c and c < WIDTH_IDX and grid[r][c] == length + 1):
-            p = findPath(newPath, r, c)
-            if len(p) != 0:
-                return p
-    # Then try moving diagonal
-    for i in range(0, 3):
-        r = row + i - 1
-        c = col + i - 1
-        if (0 <= r and r < HEIGHT_IDX and
-            0 <= c and c < WIDTH_IDX and
-            grid[r][c] == length + 1):
-            p = findPath(newPath, r, c)
-            if len(p) != 0:
-                return p
-        r = row - i + 1
-        c = col - i + 1
-        if (0 <= r and r < HEIGHT_IDX and
-            0 <= c and c < WIDTH_IDX and
-            grid[r][c] == length + 1):
-            p = findPath(newPath, r, c)
-            if len(p) != 0:
-                return p
-
-print(findPath([], 0, 0))
+start = ''
+end = ''
+startW = 'w11'
+endW = 'w41'
+nodes[startW] = ([], 0)
+while nodes[endW][1] < 0:
+    closestNewNode = ('', [], -1)
+    for key, _ in nodes.items():
+        # Get our discovered and check their connections.
+        if (nodes[key][1] != -1):
+            for edge in connections[key]:
+                edgeLen = math.sqrt((waypoints[key][0]-waypoints[edge][0])**2 +
+                    (waypoints[key][1]-waypoints[edge][1])**2)
+                dist = nodes[key][1] + edgeLen
+                # If this node is the closest and the endpoint is a point that
+                # is not yet discovered.
+                if ((closestNewNode[2] == -1 or dist < closestNewNode[2]) and
+                    nodes[edge][1] == -1):
+                    path = nodes[key][0][:]
+                    path.append(edge)
+                    closestNewNode = (edge, path, dist)
+    nodes[closestNewNode[0]] = (closestNewNode[1], closestNewNode[2])
