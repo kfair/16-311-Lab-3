1459551ea95cccd92cd2dcfbc6e968c716472623
imurdock8 <murshu3@gmail.com>
2017-05-01 18:14:19 -0400

    Updated basketball

--- Lab-10/basketball.c
+++ Lab-10/basketball.c
@@ -1,9 +1,11 @@
 task main()
 {
+
 	motor[motorB] = 50;
-	motor[motorC] = 30;
-	wait1Msec(1000);
+	motor[motorC] = -30;
+	wait1Msec(2000);
 	for(int i = 0; i < 4; i++) {
+		motor[motorC] = 0;
 		motor[motorA] = -20;
 		wait1Msec(300);
 		motor[motorA] = 0;
@@ -11,6 +13,8 @@ task main()
 		motor[motorA] = 20;
 		wait1Msec(300);
 		motor[motorA] = 0;
-		wait1Msec(500);
+		motor[motorC] = -30;
+		wait1Msec(800);
+
 	}
 }
